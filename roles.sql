9cb0893b5aa3af9c307f5e86a79714504841326a
Marcelo-012 <marcelorivero9419@gmail.com>
2025-05-07 16:37:11 -0600

    Add files via upload

---
+++ roles.sql
@@ -0,0 +1,11 @@
+ CREATE ROLE 'administrador';
+
+ GRANT ALL PRIVILEGES ON mundo.* TO 'administrador';
+ 
+ CREATE USER 'Emmanuel'@'localhost' IDENTIFIED BY 'Emmanuel012*';
+
+  GRANT 'administrador' TO 'nuevo_usuario'@'localhost';
+
+  SET DEFAULT ROLE 'administrador' TO 'Emmanuel'@'localhost';
+
+
